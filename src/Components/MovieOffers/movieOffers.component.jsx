2a5087a93b8104f5216663347270dce09dd97a9f
unknown <akshaypanchal67@gmail.com>
2021-01-14 21:31:18 -0500

    implement the MovieOffers Component

--- src/Components/MovieOffers/movieOffers.component.jsx
+++ src/Components/MovieOffers/movieOffers.component.jsx
@@ -1,8 +1,31 @@
 const MovieOffers = () => {
 
-    return(
-        <div>
-            <h1>Movie Offers are listed here!!!</h1>
+    return (
+        <div className="container">
+            <div class="card">
+                <h5 class="card-header">Offer# 1</h5>
+                <div class="card-body">
+                    <h5 class="card-title">$5</h5>
+                    <p class="card-text">see any 5 Movies in a Month</p>
+                    <a href="#" class="btn btn-primary">Buy this offer</a>
+                </div>
+            </div>
+            <div class="card">
+                <h5 class="card-header">Offer# 2</h5>
+                <div class="card-body">
+                    <h5 class="card-title">$7</h5>
+                    <p class="card-text">Get discount of 5% for all movies for a month</p>
+                    <a href="#" class="btn btn-primary">Buy this offer</a>
+                </div>
+            </div>
+            <div class="card">
+                <h5 class="card-header">Offer# 3</h5>
+                <div class="card-body">
+                    <h5 class="card-title">$20</h5>
+                    <p class="card-text">Gift Vouchers</p>
+                    <a href="#" class="btn btn-primary">Buy this offer</a>
+                </div>
+            </div>
         </div>
     );
 
--- src/Components/userTabs/userTabs.component.jsx
+++ src/Components/userTabs/userTabs.component.jsx
@@ -1,9 +1,8 @@
 import { Tabs, Tab } from 'react-bootstrap'
-import Login from '../login/login.component';
-import MovieDetails from '../movieDetails/movieDetails.component';
 import MovieList from '../MovieList/movieList.component';
 import SignUp from '../signup/signUp.component';
 import movieData from '../../data/movieList';
+import MovieOffers from '../MovieOffers/movieOffers.component';
 
 const UserTabs = () => {
 
@@ -13,7 +12,7 @@ const UserTabs = () => {
                 <MovieList movies = {movieData}/>
             </Tab>
             <Tab eventKey="Offers" title="View My Offers">
-                <Login />
+                <MovieOffers />
             </Tab>
             <Tab eventKey="past Movies" title="My Past Movie">
                 <SignUp />
